8074b0040e7d686427f86bfae2a1b33b07fa204a
blesswrld <roninfortnite71@gmail.com>
2025-04-22 18:29:26 +0300

    readonly modifiers

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 interface User {
-    login: string;
+    readonly login: string; // readonly property (свойство только для чтения)
     password: string;
     age: number;
     // addr?: string; если свойство необязательно ставим ?
-    addr: string | undefined; // если свойство обязательно, но значение может быть undefined (используем Union-Тип)
+    readonly addr: string | undefined; // если свойство обязательно, но значение может быть undefined (используем Union-Тип)
     parents?: {
         mother?: string; // если свойство необязательно ставим ?
         father?: string;
@@ -17,16 +17,40 @@ const user: User = {
     addr: "abcdefg",
 };
 
-let dbName: string;
-sendUserData(user, "dasasdas"); // Вызов функции
+// Структура дженерик принимает в себя интерфейс юзера
+const userFreeze: Readonly<User> = {
+    login: "first",
+    password: "qwerty",
+    age: 50,
+    addr: "abcdefg",
+};
 
-console.log(dbName!); // `!` — оператор утверждения ненулевого значения (Non-Null)
+// userFreeze.age = 48; Получаем ошибку тк вся структура стала readonly
+// userFreeze.password = "qwerty123"; Тут тоже самое
+
+// user.login = "dsdas"; Cannot assign to 'login' because it is a read-only property.
+
+const dbName = "12345";
 
 // Передаем объект интерфейса User в obj, и базу данных db - которая принимает в себя строку или значение undefined
 function sendUserData(obj: User, db?: string): void {
-    dbName = "12345"; // Задаем значение для глобальной переменной
-    console.log(obj.parents!.father?.toLowerCase(), db!.toLowerCase()); // `!` — оператор утверждения ненулевого значения (Non-Null), используется, когда разработчик уверен, что свойство или значение не будет `null` или `undefined` во время выполнения
+    console.log(obj.parents?.father?.toLowerCase(), db?.toLowerCase()); // `!` — оператор утверждения ненулевого значения (Non-Null), используется, когда разработчик уверен, что свойство или значение не будет `null` или `undefined` во время выполнения
 }
+
+// class Animal {
+// readonly name: string = "name"; // Фиксируем тип данных string со значением "name"
+// }
+
+// Переменная со значениями базовых портов
+// Структура дженерик
+const basicPorts: ReadonlyArray<number> = [3000, 3001, 5555]; // Делаем массив из чисел readonly
+basicPorts[0] = 5; // Получаем ошибку тк нельзя переназначать значение свойства с модификатором readonly
+basicPorts.push(566); // Тут тоже самое
+
+// Кортеж с массивами
+// const basicPorts: readonly [number, ...string[]] = [3000, "3001", "5555"];
+// basicPorts[0] = 5;
+// basicPorts.push(566);
 
 // tsc index.ts (команда в терминале для запуска компилятора ts кода)
 // tsc -help (команда в терминале для помощи с настройками)
